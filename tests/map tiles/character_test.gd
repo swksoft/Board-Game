593d22bd7055a667e864ead40faf7137b2760b73
swksoft <esparzapatricio95@gmail.com>
2024-07-05 12:14:40 -0300

    player can move in panels (uuuuu)

--- tests/map tiles/character_test.gd
+++ tests/map tiles/character_test.gd
@@ -5,30 +5,32 @@ var grid_size = 32
 
 var mouse_inside_area = false
 
+var panel_layer = 1
+
 @onready var tile_map = get_parent().get_node("TileMap")
 
 func _ready():
-	var area = Area2D.new()
-	var collision_shape = CollisionShape2D.new()
-	var shape = RectangleShape2D.new()
-	shape.extents = Vector2(grid_size / 2, grid_size / 2)
-	collision_shape.shape = shape
-	area.add_child(collision_shape)
-	area.position = position + Vector2.RIGHT * grid_size
-	#area.position = position + offset * grid_size
-	area.input_event.connect(_on_move_area)
-	add_child(area)
+	var current_tile: Vector2i = tile_map.local_to_map(global_position)
+	print(current_tile)
 
-func _process(delta):
+func _process(_delta):
 	if mouse_inside_area:
 		if Input.is_action_just_pressed("click"):
 			print_debug("Character Clicked")
+
+func _input(event):
+	var target_tile = tile_map.local_to_map(get_global_mouse_position())
+	var tile_data: TileData = tile_map.get_cell_tile_data(panel_layer, target_tile)
+	if tile_data == null: return
+	
+	var is_walkable = tile_data.get_custom_data("Square")
+	
+	if Input.is_action_just_pressed("click") and is_walkable:
+		print(target_tile)
+		position = tile_map.map_to_local(target_tile)
 
 func _on_area_2d_mouse_entered():
 	mouse_inside_area = true
 
 func _on_area_2d_mouse_exited():
 	mouse_inside_area = false
-
-func _on_move_area(viewport, event, shape_idx, area_position):
-	print("a")
--- tests/map tiles/tile_map.gd
+++ tests/map tiles/tile_map.gd
@@ -1,13 +1,18 @@
 extends TileMap
 
-func _process(delta):
-	if get_used_cells(3).size() != 0:
-		erase_cell(3, get_used_cells(3)[0])
+var select_layer = 3
+
+func _process(_delta):
+	if get_used_cells(select_layer).size() != 0:
+		erase_cell(select_layer, get_used_cells(select_layer)[0])
 	
 	var current_tile = local_to_map(get_global_mouse_position())
 	var tile_data : TileData = get_cell_tile_data(1, current_tile)
-	if tile_data != null:
+	
+	if tile_data:
 		var is_square = tile_data.get_custom_data("Square")
 		if is_square:
-			set_cell(3, current_tile, 0, Vector2(2,50), 0)
+			set_cell(select_layer, current_tile, 0, Vector2(2,50), 0)
+			
+		
 	
