868b5a50f1f04b975edcfdb7fa67605ac244838c
swksoft <esparzapatricio95@gmail.com>
2024-07-13 13:24:17 -0300

    turnos manager en proceso XQ

--- tests/test_encounter2/combat_manager.gd
+++ tests/test_encounter2/combat_manager.gd
@@ -3,16 +3,26 @@ class_name CombatManager
 
 signal turn_advanced()
 signal update_information(text: String)
+signal characters_ready
+signal update_lifebar(value : int)
+signal update_turns(value: int)
 
 @export var characters : Dictionary = {}
+@export var max_turns : int = 5
+
+var total_turns : int
 
 func _ready():
-	update_information.emit("Test.\n")
+	total_turns = max_turns
+	
+	#update_information.emit("Test.\n")
 	create_character("Hero", 100, 20, 10, 30)
 	create_character("Mage", 80, 25, 5, 25)
 	create_character("Warrior", 120, 15, 20, 1)
 	
-	#print_available_characters()
+	emit_signal("characters_ready")
+	emit_signal("update_lifebar", get_total_health())
+	emit_signal("update_turns", total_turns)
 	
 func create_character(character_name : String, hp : int, atk : int, dfs : int, eva : int):
 	var character_data = {
@@ -24,13 +34,14 @@ func create_character(character_name : String, hp : int, atk : int, dfs : int, e
 	characters[character_name] = character_data
 	return character_data
 
-func get_character(char_name: String):
-	if characters.has(char_name):
-		return characters[char_name]
-	else:
-		return null
+func get_total_health():
+	var total_health = 0
+	for character_data in characters.values():
+		total_health += character_data["hp"]
+	return total_health
 
-func print_available_characters():
-	for char_name in characters.keys():
-		var character_data = characters[char_name]
-		print("Character: ", char_name, " - Data: ", character_data)
+
+func _on_attack_button_pressed():
+	total_turns -= 1
+	emit_signal("update_turns", total_turns)
+	
--- tests/test_encounter2/ui/UI.gd
+++ tests/test_encounter2/ui/UI.gd
@@ -2,10 +2,63 @@ extends Control
 
 @export var combat_manager : CombatManager
 
+@export var sprite_turn : Resource 
+
+func update_turns(value):
+	var child_number = $VBoxContainer/Upper/HBoxContainer.get_children()
+	print(child_number)
+	if child_number.size() > 0:
+		for child in child_number.size():
+			child_number[child].queue_free()
+		
+	for turn in combat_manager.total_turns:
+		var texture_container = TextureRect.new()
+		texture_container.texture = sprite_turn
+		$VBoxContainer/Upper/HBoxContainer.add_child(texture_container, true, Node.INTERNAL_MODE_FRONT)
+
 func _ready():
-	var characters = combat_manager.characters
-	_on_combat_manager_update_information(characters)
+	#update_turns()
+	
+	combat_manager.characters_ready.connect(_on_combat_manager_characters_ready)
+	combat_manager.update_lifebar.connect(_on_update_lifebar)
+	combat_manager.update_turns.connect(_on_update_turns)
+	
+func update_information(info: String):
+	$VBoxContainer/Center/PanelContainer/RichTextLabel.append_text(info)
 	
 func _on_combat_manager_update_information(text):
 	$VBoxContainer/Center/PanelContainer/RichTextLabel.append_text(text)
 
+func _on_update_turns(value):
+	update_turns(value)
+
+func _on_combat_manager_characters_ready():
+	var characters = combat_manager.characters
+	
+	if characters.size() > 0:
+		var index = 1  
+		
+		for char_name in characters.keys():
+			var character_data = characters[char_name]
+			
+			var path_name = "VBoxContainer/Bottom/Char%d/VBoxContainer/Label" % index
+			var path_hp  = "VBoxContainer/Bottom/Char%d/VBoxContainer/GridContainer/Label2" % index
+			var path_atk = "VBoxContainer/Bottom/Char%d/VBoxContainer/GridContainer/Label4" % index
+			var path_dfs = "VBoxContainer/Bottom/Char%d/VBoxContainer/GridContainer/Label6" % index
+			var path_eva = "VBoxContainer/Bottom/Char%d/VBoxContainer/GridContainer/Label8" % index
+			
+			if has_node(path_name) and has_node(path_hp) and has_node(path_atk) and has_node(path_dfs) and has_node(path_eva):
+				get_node(path_name).text = char_name
+				get_node(path_hp).text = str(character_data["hp"])
+				get_node(path_atk).text = str(character_data["atk"])
+				get_node(path_dfs).text = str(character_data["dfs"])
+				get_node(path_eva).text = str(character_data["eva"])
+				
+			index += 1
+	else:
+		print("No characters available.")
+
+func _on_update_lifebar(value):
+	$VBoxContainer/Bottom/Center/ProgressBar.max_value = value
+	$VBoxContainer/Bottom/Center/ProgressBar.value = value
+	$VBoxContainer/Bottom/Center/ProgressBar/Label.text = str(value) + "/" + str(value)
